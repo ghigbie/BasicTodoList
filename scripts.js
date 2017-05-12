933223459d95ee2f57ff0100135c9e31cb1156fc
George Higbie <gehigbie@gmail.com>
2017-05-12 15:42:25 -0700

    Add manner to store elements in dom

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const inputEl = document.querySelector("input");
 const buttonEl = document.querySelector("button");
 const ulEl = document.querySelector("ul");
+let todos = [];
 
 buttonEl.addEventListener("click", addTodo);
 
@@ -10,14 +11,20 @@ function addTodo(){
 	if(userInput.trim() == ""){
 		return;
 	}
+	let newTodo = {id: Math.random(), value: userInput};
+	todos.push(newTodo);
+	console.log(todos);
 	let todoLi = document.createElement("li");
 	todoLi.textContent = userInput;
 	todoLi.addEventListener("click", removeTodo);
+	todoLi.dataset.id = newTodo.id;
 	ulEl.appendChild(todoLi);
 	inputEl.value = "";
+
 }
 
 function removeTodo(event){
-	var clickedLi = event.target;
+	let clickedLi = event.target;
+	let itemId = clickedLi.dataset.id;
 	clickedLi.parentNode.removeChild(clickedLi);
 }

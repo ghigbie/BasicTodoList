c9c2f80c947e863533c7f7e22ac9548eccdeadbd
George Higbie <gehigbie@gmail.com>
2017-05-12 15:52:32 -0700

    Add jquery via cdn

--- scripts.js
+++ scripts.js
@@ -26,5 +26,12 @@ function addTodo(){
 function removeTodo(event){
 	let clickedLi = event.target;
 	let itemId = clickedLi.dataset.id;
+	for(let i = 0; i < todos.length; i++){
+		if(todos[i].id == itemId){
+			todos.splice(i, 1);
+			break;
+		}
+	}
+	console.log(todos);
 	clickedLi.parentNode.removeChild(clickedLi);
 }
